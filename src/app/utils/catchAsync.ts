5646c7d127f47e0618dd845c1d60da37c3d24be2
shamim-hossain008 <shamim.hossain3573@gmail.com>
2025-08-30 00:40:13 +0800

    Create model for Tour and Tour Types

--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -8,9 +8,8 @@ type AsyncHandler = (
 
 export const catchAsync =
   (fn: AsyncHandler) => (req: Request, res: Response, next: NextFunction) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     Promise.resolve(fn(req, res, next)).catch((err: any) => {
-      console.log(err);
       next(err);
     });
   };
- 
